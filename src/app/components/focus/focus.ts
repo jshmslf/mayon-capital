627f5701237246ffe859f50093ceacd054dd09b0
jshmslf <jshmslf@gmail.com>
2025-10-21 00:10:37 +0800

    fixed img

--- src/app/components/focus/focus.ts
+++ src/app/components/focus/focus.ts
@@ -11,17 +11,17 @@ import { CommonModule } from '@angular/common';
 export class FocusComponent {
   focusItems = [
     {
-      img: '/assets/deep_tech.png',
+      img: './assets/deep_tech.png',
       title: 'Deep Tech',
       desc: 'We remain at the forefront of driving innovation in the domain of AI/ML, Aerospace, Robotics, MedTech, FarmTech, Biotech and Web3.0, continually propelling these fields forward.'
     },
     {
-      img: '/assets/wealth.png',
+      img: './assets/wealth.png',
       title: 'Wealth',
       desc: 'Our astute investment methodology has proven to be a potent catalyst for a substantial enhancement in the net worth of our valued partners, achieving remarkable results in a remarkably brief span of time.'
     },
     {
-      img: '/assets/infrastructure.png',
+      img: './assets/infrastructure.png',
       title: 'Infrastructure',
       desc: 'We boast a well-diversified portfolio of infrastructure projects spanning the globe, encompassing a wide spectrum of ventures such as communication infrastructure, commercial development and housing projects.'
     }
