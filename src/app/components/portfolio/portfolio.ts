24c6adf6553555e234655a8854eb28f42ea49df1
jshmslf <jshmslf@gmail.com>
2025-10-21 07:23:35 +0800

    4

--- src/app/components/portfolio/portfolio.ts
+++ src/app/components/portfolio/portfolio.ts
@@ -10,13 +10,13 @@ import { CommonModule } from '@angular/common';
 })
 export class PortfolioComponent {
   portfolioItems = [
-    { src: '/assets/logos/onflow.png', alt: 'OnFlow' },
-    { src: '/assets/logos/farmatics.png', alt: 'Farmatics' },
-    { src: '/assets/logos/knowz.png', alt: 'Knowz' },
-    { src: '/assets/logos/mogo.png', alt: 'Mogo' },
-    { src: '/assets/logos/mober.png', alt: 'Mober' },
-    { src: '/assets/logos/feather-dynamics.png', alt: 'Feather Dynamics' },
-    { src: '/assets/logos/wpl.png', alt: 'WPL' },
-    { src: '/assets/logos/mayonventures.png', alt: 'Mayon Ventures' }
+    { src: './assets/logos/onflow.png', alt: 'OnFlow' },
+    { src: './assets/logos/farmatics.png', alt: 'Farmatics' },
+    { src: './assets/logos/knowz.png', alt: 'Knowz' },
+    { src: './assets/logos/mogo.png', alt: 'Mogo' },
+    { src: './assets/logos/mober.png', alt: 'Mober' },
+    { src: './assets/logos/feather-dynamics.png', alt: 'Feather Dynamics' },
+    { src: './assets/logos/wpl.png', alt: 'WPL' },
+    { src: './assets/logos/mayonventures.png', alt: 'Mayon Ventures' }
   ];
 }
