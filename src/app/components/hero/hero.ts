49da64ae2e646a88a3b270917f21d04c6517011a
jshmslf <jshmslf@gmail.com>
2025-10-21 07:34:44 +0800

    5

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hero.html',
   styleUrl: './hero.css'
 })
 export class Hero {
+  heroBackground = {
+    'background-image': `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('${this.getBackgroundUrl()}')`,
+    'background-size': 'cover',
+    'background-position': 'center',
+    'background-repeat': 'no-repeat',
+    'background-attachment': 'fixed'
+  };
+
+  getBackgroundUrl() {
+    return '/background.png';
+  }
 
 }
